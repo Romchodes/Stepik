7cf6de2e4818352a325589f9ee6de7f1a89fe978
Romchodes <romchodes@gmail.com>
2023-04-27 01:31:15 +0300

    True or False

--- Stepik4.py
+++ Stepik4.py
@@ -2,6 +2,10 @@ x = 5
 y = 10
 print(y > x * x or y >= 2 * x and x < y)
 
+a = True
+b = False
+print(a and b or not a and not b)
+
 print (True and False)
 print (False and True)
 print (True or False)
